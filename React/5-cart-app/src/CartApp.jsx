cad9c619642653874094ef424a02f77a7d5c253d
Fran94 <francisco.diaz.s@usach.cl>
2025-10-06 18:48:14 -0300

    Quinto ejercicio completo

---
+++ React/5-cart-app/src/CartApp.jsx
@@ -0,0 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { CartView } from "./components/CartView"
+import { CatalogView } from "./components/CatalogView"
+import { useItemsCart } from "./hooks/useItemsCart"
+import { Navbar } from "./components/Navbar";
+import { CartRoutes } from "./routes/CartRoutes";
+
+
+
+export const CartApp = () => {
+
+    const { cartItems, handlerAddProductCart, handlerDeleteProductCart } = useItemsCart();
+
+    return (
+        <>
+            <Navbar />
+            <div className="container my-4">
+                <h3>CartApp</h3>
+                <CartRoutes cartItems={cartItems}
+                    handlerAddProductCart={handlerAddProductCart}
+                    handlerDeleteProductCart={handlerDeleteProductCart} />
+            </div>
+        </>
+    )
+}
